c097549a270c48abb6e495f086eb211a1c20b650
kaisparkle <kaisparkle1@gmail.com>
2020-04-07 07:20:00 +1200

    Stub userSvc and userSvc::GetRedeemTokens.

---
+++ Server/Node/Services/UserSvc/userSvc.cs
@@ -0,0 +1,20 @@
+﻿using Common.Logging;
+using Common.Services;
+using PythonTypes.Types.Complex;
+using PythonTypes.Types.Primitives;
+
+namespace Node.Services.UserSvc
+{
+    public class userSvc : Service
+    {
+        public userSvc(ServiceManager manager) : base(manager)
+        {
+
+        }
+
+        public PyList GetRedeemTokens(PyDictionary namedPayload, Client client)
+        {
+            return null;
+        }
+    }
+}
