2287db6a22ee14e472a8e3d898720ec5dc0842b0
Alexis Maiquez Murcia <almamu@almamu.com>
2021-01-20 12:19:16 +0100

    + added LookupCharacters to the lookupSvc
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

--- Server/Node/Services/Data/LookupDB.cs
+++ Server/Node/Services/Data/LookupDB.cs
@@ -7,6 +7,36 @@ namespace Node.Services.Data
 {
     public class LookupDB : DatabaseAccessor
     {
+        public Rowset LookupCharacters(string namePart, bool exact)
+        {
+            if (exact == true)
+            {
+                return Database.PrepareRowsetQuery(
+                    "SELECT characterID, itemName AS characterName, typeID " +
+                    "FROM chrInformation " +
+                    "LEFT JOIN entity ON characterID = itemID " +
+                    $"WHERE itemName = @namePart",
+                    new Dictionary<string, object>()
+                    {
+                        {"@namePart", namePart}
+                    }
+                );                
+            }
+            else
+            {
+                return Database.PrepareRowsetQuery(
+                    "SELECT characterID, itemName AS characterName, typeID " +
+                    "FROM chrInformation " +
+                    "LEFT JOIN entity ON characterID = itemID " +
+                    $"WHERE itemName LIKE @namePart",
+                    new Dictionary<string, object>()
+                    {
+                        {"@namePart", namePart + "%"}
+                    }
+                );
+            }
+        }
+        
         public Rowset LookupPlayerCharacters(string namePart, bool exact)
         {
             if (exact == true)
--- Server/Node/Services/Data/lookupSvc.cs
+++ Server/Node/Services/Data/lookupSvc.cs
@@ -13,19 +13,16 @@ namespace Node.Services.Data
             this.DB = db;
         }
 
-        public PyDataType LookupPlayerCharacters(PyString criteria, PyInteger exactMatch, CallInformation call)
+        public PyDataType LookupPlayerCharacters(PyString criteria, PyDataType exactMatch, CallInformation call)
         {
-            return this.LookupPlayerCharacters(criteria, exactMatch == 1);
-        }
+            bool exact = false;
 
-        public PyDataType LookupPlayerCharacters(PyString criteria, PyBool exactMatch, CallInformation call)
-        {
-            return this.LookupPlayerCharacters(criteria, exactMatch);
-        }
-        
-        private PyDataType LookupPlayerCharacters(string criteria, bool exactMatch)
-        {
-            return this.DB.LookupPlayerCharacters(criteria, exactMatch);
+            if (exactMatch is PyBool exactBool)
+                exact = exactBool;
+            else if (exactMatch is PyInteger exactInteger)
+                exact = exactInteger != 0;
+            
+            return this.DB.LookupPlayerCharacters(criteria, exact);
         }
 
         public PyDataType LookupOwners(PyString criteria, PyDataType exactMatch, CallInformation call)
@@ -38,6 +35,18 @@ namespace Node.Services.Data
                 exact = exactInteger != 0;
             
             return this.DB.LookupOwners(criteria, exact);
+        }
+
+        public PyDataType LookupCharacters(PyString criteria, PyDataType exactMatch, CallInformation call)
+        {
+            bool exact = false;
+
+            if (exactMatch is PyBool exactBool)
+                exact = exactBool;
+            else if (exactMatch is PyInteger exactInteger)
+                exact = exactInteger != 0;
+            
+            return this.DB.LookupCharacters(criteria, exact);
         }
     }
 }
