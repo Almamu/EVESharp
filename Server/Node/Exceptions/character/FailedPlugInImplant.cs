6479e8ba962ea73adbbad27ef0efcdaa446763f8
Alexis Maiquez Murcia <almamu@almamu.com>
2021-02-15 02:21:09 +0100

    + added missing file in the last commit
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

---
+++ Server/Node/Exceptions/character/FailedPlugInImplant.cs
@@ -0,0 +1,13 @@
+using Node.Inventory.Items;
+using PythonTypes.Types.Exceptions;
+using PythonTypes.Types.Primitives;
+
+namespace Node.Exceptions.character
+{
+    public class FailedPlugInImplant : UserError
+    {
+        public FailedPlugInImplant() : base("FailedPlugInImplant")
+        {
+        }
+    }
+}
