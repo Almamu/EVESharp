cc39fedf3aa0cc6b0080defc2a42c6815eae8e7a
Alexis Maiquez Murcia <almamu@almamu.com>
2021-04-23 03:18:50 +0200

    ~ changed the formating of the eve mail window to show the date and the title closer and the body of the message more separated
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

--- Tools/EveLivePatches/inboxPatches.py
+++ Tools/EveLivePatches/inboxPatches.py
@@ -4,7 +4,7 @@ def ShowMessage(self, messageID, attachments=[]):
         return
     (msg, attachments) = self.GetMessage(messageID)
     wnd.sr.viewmessageform.parent.state = uix.UI_PICKCHILDREN
-    wnd.sr.viewmessageform.SetValue('<h1>' + msg.subject + '</h1><br>' + util.FmtDate(msg.created, 'ls') + '<br>' + msg.body, scrolltotop=1, preformatted=1)
+    wnd.sr.viewmessageform.SetValue('<h1>' + msg.subject + '</h1>' + util.FmtDate(msg.created, 'ls') + '<br><br>' + msg.body, scrolltotop=1)
     uix.Flush(wnd.sr.attachments)
     wnd.sr.attachments.state = uix.UI_HIDDEN
     self.showing_message = messageID
