03204c1f3ffe70dcb6c4564abd4695e6c5bc0770
Alexis Maiquez Murcia <almamu@almamu.com>
2022-08-09 15:13:17 +0200

    Added tests for wallet restrictions
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

--- Server/EVESharp.Node.Unit/MarketTests/WalletTests.cs
+++ Server/EVESharp.Node.Unit/MarketTests/WalletTests.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using EVESharp.Database;
 using EVESharp.EVE.Data.Corporation;
 using EVESharp.EVE.Data.Market;
+using EVESharp.EVE.Exceptions;
 using EVESharp.EVE.Market;
 using EVESharp.EVE.Notifications;
 using EVESharp.EVE.Notifications.Wallet;
@@ -27,6 +28,7 @@ public class WalletTests
     private Harmony mHarmony;
 
     private static double sCurrentBalance;
+    private static double sExpectedBalance;
     private static int    sCurrentOwnerID;
     private static int    sCurrentWalletKey;
     
@@ -34,6 +36,9 @@ public class WalletTests
     static bool MktWalletGetBalance (IDatabaseConnection Database, ref IDbConnection connection, int walletKey, int ownerID, ref double __result)
     {
         __result = sCurrentBalance;
+
+        Assert.AreEqual (sCurrentWalletKey, walletKey);
+        Assert.AreEqual (sCurrentOwnerID, ownerID);
         
         return false;
     }
@@ -41,9 +46,32 @@ public class WalletTests
     [HarmonyPatch(typeof(WalletDB), nameof(WalletDB.MktWalletSetBalance))]
     static bool MktWalletSetBalance (IDatabaseConnection Database, ref IDbConnection connection, double balance, int ownerID, int walletKey)
     {
+        Assert.AreEqual (sCurrentWalletKey, walletKey);
+        Assert.AreEqual (sCurrentOwnerID,   ownerID);
+        Assert.AreEqual (sExpectedBalance,  balance);
+        
         return false;
     }
-    
+
+    [HarmonyPatch(typeof(WalletDB), nameof(WalletDB.MktRecordTransaction))]
+    static bool MktRecordTransaction
+    (
+        IDatabaseConnection Database, int ownerID,   TransactionType type, int characterID, int otherID, int typeID, int quantity,
+        double                   amount,   int stationID, int             walletKey
+    )
+    {
+        return false;
+    }
+
+    [HarmonyPatch(typeof(WalletDB), nameof(WalletDB.MktCreateJournalEntry))]
+    static bool MktCreateJournalEntry
+    (
+        IDatabaseConnection Database,     MarketReference reference, int characterID, int ownerID1, int? ownerID2, int? referenceID, double amount,
+        double                   finalBalance, string          reason,    int walletKey
+    )
+    {
+        return false;
+    }
     
     [SetUp]
     public void SetUp ()
@@ -60,16 +88,16 @@ public class WalletTests
         this.mHarmony.UnpatchAll ();
     }
 
-    [TestCase(14000, WalletKeys.MAIN,    false, 1500, 100)]
-    [TestCase(14000, WalletKeys.MAIN,    false, 1500, 1500)]
-    [TestCase(14000, WalletKeys.MAIN,    true,  1500, 100)]
-    [TestCase(14000, WalletKeys.SECOND,  true,  1500, 1500)]
-    [TestCase(14000, WalletKeys.THIRD,   true,  1500, 100)]
-    [TestCase(14000, WalletKeys.FOURTH,  true,  1500, 1500)]
-    [TestCase(14000, WalletKeys.FIFTH,   true,  1500, 100)]
-    [TestCase(14000, WalletKeys.SIXTH,   true,  1500, 1500)]
-    [TestCase(14000, WalletKeys.SEVENTH, true,  1500, 1500)]
-    public void WalletTest (int ownerID, int walletKey, bool isCorporation, double balance, double request)
+    [TestCase(14000, WalletKeys.MAIN,    false, 1500, 100,  1400)]
+    [TestCase(14000, WalletKeys.MAIN,    false, 1500, 1500, 0)]
+    [TestCase(14000, WalletKeys.MAIN,    true,  1500, 100,  1400)]
+    [TestCase(14000, WalletKeys.SECOND,  true,  1500, 1500, 0)]
+    [TestCase(14000, WalletKeys.THIRD,   true,  1500, 100,  1400)]
+    [TestCase(14000, WalletKeys.FOURTH,  true,  1500, 1500, 0)]
+    [TestCase(14000, WalletKeys.FIFTH,   true,  1500, 100,  1400)]
+    [TestCase(14000, WalletKeys.SIXTH,   true,  1500, 1500, 0)]
+    [TestCase(14000, WalletKeys.SEVENTH, true,  1500, 1500, 0)]
+    public void WalletTest (int ownerID, int walletKey, bool isCorporation, double balance, double request, double expectedBalance)
     {
         if (isCorporation == false)
         {
@@ -88,15 +116,33 @@ public class WalletTests
             ));    
         }
 
-        sCurrentBalance = balance;
+        sCurrentBalance   = balance;
+        sExpectedBalance  = expectedBalance;
+        sCurrentOwnerID   = ownerID;
+        sCurrentWalletKey = walletKey;
         
         // obtain the wallet
         using (IWallet wallet = this.mWallets.AcquireWallet (ownerID, walletKey, isCorporation))
         {
-            Assert.DoesNotThrow (() => wallet.EnsureEnoughBalance (request));            
+            Assert.DoesNotThrow (() => wallet.EnsureEnoughBalance (request));     
+            wallet.CreateJournalRecord (MarketReference.Bounty, 0, null, null, -request);
         }
         
         this.mDatabaseMock.Verify ();
         this.mDatabaseMock.VerifyNoOtherCalls ();
+    }
+
+    [TestCase(14000, 1500, 150000)]
+    [TestCase(14000, 1500, 1500.1)]
+    public void WalletErrorTest (int ownerID, double balance, double request)
+    {
+        sCurrentBalance   = balance;
+        sCurrentOwnerID   = ownerID;
+        sCurrentWalletKey = WalletKeys.MAIN;
+        
+        using (IWallet wallet = this.mWallets.AcquireWallet (ownerID, WalletKeys.MAIN, false))
+        {
+            Assert.Throws <NotEnoughMoney> (() => wallet.EnsureEnoughBalance (request));
+        }
     }
 }
