0ecce28861c853fb8bf0f83371d9462c6e4a6299
Alexis Maiquez Murcia <almamu@almamu.com>
2022-12-13 21:25:15 +0100

    Change GitHub actions to run debug tests instead of release until Harmony supports .NET 7
    Signed-off-by: Alexis Maiquez Murcia <almamu@almamu.com>

--- Server/EVESharp.Node.Unit/Utils/HarmonyExtensions.cs
+++ Server/EVESharp.Node.Unit/Utils/HarmonyExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 
@@ -11,6 +12,11 @@ public static class HarmonyExtensions
     {
         harmony.Setup (test.GetType ());
     }
+
+    public static void Setup <T> (this Harmony harmony)
+    {
+        harmony.Setup (typeof (T));
+    }
     
     public static void Setup (this Harmony harmony, Type test)
     {
@@ -19,33 +25,35 @@ public static class HarmonyExtensions
 
         foreach (MethodInfo method in methods)
         {
-            foreach (HarmonyPatch patch in method.GetCustomAttributes <HarmonyPatch> ())
+            HarmonyPatch patch = method.GetCustomAttributes <HarmonyPatch> ().SingleOrDefault (defaultValue: null);
+
+            if (patch is null)
+                continue;
+            
+            // build the parameter list (ignoring special parameters)
+            ParameterInfo [] parameters = method.GetParameters ();
+            List <Type>      types      = new List <Type> ();
+
+            foreach (ParameterInfo parameter in parameters)
             {
-                // build the parameter list (ignoring special parameters)
-                ParameterInfo [] parameters = method.GetParameters ();
-                List <Type>      types      = new List <Type> ();
-
-                foreach (ParameterInfo parameter in parameters)
+                switch (parameter.Name)
                 {
-                    switch (parameter.Name)
-                    {
-                        case "__instance":
-                        case "__result":
-                        case "__args":
-                        case "__originalMethod":
-                        case "__runOriginal":
-                        case "__state":
-                            break;
-                        default:
-                            types.Add (parameter.ParameterType);
-                            break;
-                    }
+                    case "__instance":
+                    case "__result":
+                    case "__args":
+                    case "__originalMethod":
+                    case "__runOriginal":
+                    case "__state":
+                        break;
+                    default:
+                        types.Add (parameter.ParameterType);
+                        break;
                 }
-
-                MethodInfo tmp = AccessTools.Method (patch.info.declaringType, patch.info.methodName, types.ToArray ());
-                
-                harmony.Patch (tmp, new HarmonyMethod (method));
             }
+
+            MethodInfo tmp = AccessTools.Method (patch.info.declaringType, patch.info.methodName, types.ToArray ());
+            
+            harmony.Patch (tmp, new HarmonyMethod (method));
         }
     }
 }
